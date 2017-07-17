5ff276fbe7a015dc6e57b2501b102a151e5bc89d
Aldo Velmeza <aldovelmeza@gmail.com>
2017-07-17 11:59:15 -0500

    Update n_uptask.py

--- n_uptask.py
+++ n_uptask.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+#
+#  INTERSYS.py
+#
+#  Copyright 2017 AVELAZCX <aldo.alfonsox.velasco.meza@intel.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #################################################################################################
 #
 #  uptask.py -- Update a Task identified by the FormattedID value
